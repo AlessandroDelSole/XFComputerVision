741ab8daf1b4a4bba9810bd0bd65dc449cddcff6
Alessandro Del Sole <alessandro.delsole@visual-basic.it>
2018-03-01 19:31:21 +0100

    Add project files.

---
+++ XFComputerVision/XFComputerVision/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Vision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XFComputerVision
+{
+	public partial class App : Application
+	{
+        internal static VisionServiceClient visionClient;
+        internal static EmotionServiceClient emotionClient;
+
+        public App ()
+		{
+			InitializeComponent();
+
+            visionClient = new VisionServiceClient("YOUR-KEY-GOES-HERE",
+    "https://westeurope.api.cognitive.microsoft.com/vision/v1.0");
+
+            emotionClient = new EmotionServiceClient("YOUR-KEY-GOES-HERE",
+                            "https://westus.api.cognitive.microsoft.com/emotion/v1.0");
+
+            MainPage = new XFComputerVision.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ XFComputerVision/XFComputerVision/EmotionPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.ProjectOxford.Common.Contract;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XFComputerVision;
+
+namespace XFComputerVision
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmotionPage : ContentPage
+    {
+        public EmotionPage()
+        {
+            InitializeComponent();
+        }
+
+        private async Task<IEnumerable<string>> RecognizeEmotionAsync(Stream imageFile)
+        {
+            // Get emotions from the specified stream
+            Emotion[] emotionResult = await App.emotionClient.RecognizeAsync(imageFile);
+            // Assuming the picture has one face, retrieve emotions for the
+            // first item in the returned array
+            var faceEmotion = emotionResult[0]?.Scores.ToRankedList();
+
+            var emotions = new List<string>();
+
+            foreach(var emotion in faceEmotion)
+            {
+                emotions.Add($"{emotion.Key}, {emotion.Value}");
+            }
+
+            return emotions.AsEnumerable();
+        }
+
+        private async void UploadPictureButton_Clicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("No upload", "Picking a photo is not supported.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+            if (file == null)
+                return;
+
+            this.Indicator1.IsVisible = true;
+            this.Indicator1.IsRunning = true;
+            Image1.Source = ImageSource.FromStream(() => file.GetStream());
+
+            try
+            {
+                this.BindingContext = await RecognizeEmotionAsync(file.GetStream());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+                return;
+            }
+            finally
+            {
+                this.Indicator1.IsRunning = false;
+                this.Indicator1.IsVisible = false;
+            }
+        }
+
+        private async void TakePictureButton_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", "No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                SaveToAlbum = true,
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            this.Indicator1.IsVisible = true;
+            this.Indicator1.IsRunning = true;
+
+            Image1.Source = ImageSource.FromStream(() => file.GetStream());
+            this.BindingContext = await RecognizeEmotionAsync(file.GetStream());
+
+            this.Indicator1.IsRunning = false;
+            this.Indicator1.IsVisible = false;
+        }
+    }
+}
